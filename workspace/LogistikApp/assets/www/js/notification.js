272b50d5af8d80632e04c9ca9ffbe94d9319b1fd
Norbert <nschmidbartl@gmx.de>
2014-12-06 20:31:30 +0100

    cs notifications

---
+++ workspace/LogistikApp/assets/www/js/notification.js
@@ -0,0 +1,22 @@
+/** * notification.js.
+ *
+ * >>Description<<
+ *
+ * @author Norbert
+ * @date 06.12.14 - 20:28
+ * @copyright  */
+
+
+
+var notifications = {
+
+
+    show: function(message, callback) {
+
+
+
+    }
+
+
+
+}
