6166ad8b14ac3094e8c0492ddd0b98eb851a30a2
cheriott <benedikt_n@gmx.de>
2014-12-28 23:44:21 +0100

    termin

--- workspace/LogistikApp/assets/www/js/termin.js
+++ workspace/LogistikApp/assets/www/js/termin.js
@@ -4,10 +4,40 @@ var termin = {
 
     initialize: function() {
 
+
         $('#popupTermin .clockpicker').clockpicker();
 
+
+
+        $('.clockpicker').clockpicker()
+            .find('input').change(function(){
+                console.log(this.value);
+            });
+        var input = $('#single-input').clockpicker({
+            placement: 'bottom',
+            align: 'left',
+            autoclose: true,
+            'default': 'now'
+        });
+
+
+
+        // Manually toggle to the minutes view
+        $('#check-minutes').click(function(e){
+            // Have to stop propagation here
+            e.stopPropagation();
+            input.clockpicker('show')
+                .clockpicker('toggleView', 'minutes');
+        });
+        if (/mobile/i.test(navigator.userAgent)) {
+            $('input').prop('readOnly', true);
+        }
+
+
         $("#cb_neuer_termin").click(function () {
+            alert("termin eintragen");
             $("#terminEintragen").show();
+
             $("#termine_menu").hide();
 
         })
