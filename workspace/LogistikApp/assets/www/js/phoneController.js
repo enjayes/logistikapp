1412ac92c1150f23e98cbcebe52fc38e10b9fa64
Norbert <nschmidbartl@gmx.de>
2014-12-29 17:14:47 +0100

    ca: sms + phone call

--- workspace/LogistikApp/assets/www/js/phoneController.js
+++ workspace/LogistikApp/assets/www/js/phoneController.js
@@ -15,27 +15,29 @@ var phoneController = {
     },
 
     informAboutLogin:function(markt,lieferant){
-        var name = "Unbekannt";
-        var firma = "Unbekannt"
-        var telefon = "Unbekannt"
-        if(lieferant) {
-            if (lieferant.vorname) {
-                name = lieferant.vorname + " "
-            }
-            if (lieferant.name) {
-                name = name + lieferant.name + " "
-            }
-            if (lieferant.firma) {
-                firma = lieferant.firma
-            }
-            if (lieferant.telefon) {
-                telefon = lieferant.telefon
-            }
-            if (markt.sms) {
-                phoneController.sendMessage(markt.telefon, "Ein Lieferant hat sich soeben am Terminal angemeldet.\n\nName: " + name + "\nFirma: " + firma + "\nTel: " + telefon);
-            }
-            if (markt.call) {
-                phoneController.callNumber(markt.telefon, "Hallo, ein Lieferant hat sich soeben am Terminal angemeldet! Name: " + name + ".  Firma: " + firma + ".  Auf Wiederhören!");
+        if (markt.sms || markt.call) {
+            var name = "Unbekannt";
+            var firma = "Unbekannt"
+            var telefon = "Unbekannt"
+            if(lieferant) {
+                if (lieferant.vorname) {
+                    name = lieferant.vorname + " "
+                }
+                if (lieferant.name) {
+                    name = name + lieferant.name + " "
+                }
+                if (lieferant.firma) {
+                    firma = lieferant.firma
+                }
+                if (lieferant.telefon) {
+                    telefon = lieferant.telefon
+                }
+                if (markt.sms) {
+                    phoneController.sendMessage(markt.telefon, "Ein Lieferant hat sich soeben am Terminal angemeldet.\n\nName: " + name + "\nFirma: " + firma + "\nTel: " + telefon);
+                }
+                if (markt.call) {
+                    phoneController.callNumber(markt.telefon, "Hallo, ein Lieferant hat sich soeben am Terminal angemeldet! Name: " + name + ".  Firma: " + firma + ".  Auf Wiederhören!");
+                }
             }
         }
     }
