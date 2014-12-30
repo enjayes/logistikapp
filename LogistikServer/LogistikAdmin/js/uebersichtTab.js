0ed6e0348829e0dbce8f16c9bdade65388294309
manfred.schmidbartl <manfred.schmidbartl@munichdev.com>
2014-12-30 13:39:19 +0800

    style

--- LogistikServer/LogistikAdmin/js/uebersichtTab.js
+++ LogistikServer/LogistikAdmin/js/uebersichtTab.js
@@ -19,7 +19,7 @@ uebersichtTab = {
         uebersichtTab.pieAufteilung = new PieChart("#aufteilungChart",[]);
 
         var selectedList = $("#selectedAuftragsHistorieLieferanten");
-        this.selectedItemsShowHtml = "<div class='selectedLieferantShow'>Anzeigen für:</div><div class='selectedLieferantShowMargin'></div>"
+        this.selectedItemsShowHtml = "<div class='selectedLieferantShow'>Auswahl:</div><div class='selectedLieferantShowMargin'></div>"
         selectedList.click(function (event) {
             event.stopPropagation();
             $("#searchAuftragsHistorieLieferanten input").focus();
