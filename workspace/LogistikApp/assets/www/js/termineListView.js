e91d76c07d50e1e1bbe171fbdb6ff3c88861d95e
cheriott <benedikt_n@gmx.de>
2015-06-19 16:08:27 +0200

    datepicker

---
+++ workspace/LogistikApp/assets/www/js/termineListView.js
@@ -0,0 +1,17 @@
+/**
+ * Created by StandardB on 19.06.2015.
+ */
+
+var termineListView = {
+    initialize: function()
+    {
+        $("#cb_neuer_termin").click(function () {
+
+            $("#terminEintragen").show();
+
+            $("#termine_menu").hide();
+
+        });
+    }
+
+}
